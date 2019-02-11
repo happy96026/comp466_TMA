6ac2513de7f6a1b60fb878d7ae8417baa1c62068
Min Soung Choi <m.choi960@gmail.com>
2019-02-11 00:48:22 -0700

    part3 WIP

---
+++ TMA1/part3/slideshow.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    var asyncRequest = new XMLHttpRequest();
+    asyncRequest.addEventListener("readystatechange", function() {}, false);
+    //asyncRequest.open("Get", "notes/note" + unit + ".json", true);
+    //asyncRequest.send();
+});
