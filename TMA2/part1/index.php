fea6d89aa0ae5c0fddadc3f0d0a389fa66d6f1be
Min Soung Choi <m.choi960@gmail.com>
2019-02-22 01:16:10 -0700

    TMA2 part1 WIP

---
+++ TMA2/part1/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<!--<?php-->
+<!--if (session_status() == PHP_SESSION_NONE) {-->
+    <!--$button = -->
+<!--}-->
+<!--?>-->
+
+<html>
+    <head>
+        <title>Welcome!</title>
+        <link rel="stylesheet" type="text/css" href="../shared/styles.css" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto"/>
+    </head>
+
+    <body class="part1">
+        <nav>
+            <div>
+                <a href="/part1" id="banner">Bookmarker</a>
+            </div>
+            <div>
+                <div class="dropdown">
+                    <button class="nav-button">Log in</button>
+                    <!--<button class="nav-button">Profile</button>-->
+                    <!--<ul class="dropdown-content">-->
+                        <!--<li>Edit bookmarks</li>-->
+                        <!--<li>Log out</li>-->
+                    <!--</ul>-->
+                </div>
+            </div>
+        </nav>
+        <div class="content">
+            <div id="top10">
+                <h1>Top 10 Bookmarks</h1>
+                <ol class="box">
+                    <li><a href="#">Title</a></li>
+                    <li><a href="#">Title</a></li>
+                    <li><a href="#">Title</a></li>
+                    <li><a href="#">Title</a></li>
+                    <li><a href="#">Title</a></li>
+                    <li><a href="#">Title</a></li>
+                    <li><a href="#">Title</a></li>
+                    <li><a href="#">Title</a></li>
+                    <li><a href="#">Title</a></li>
+                    <li><a href="#">Title</a></li>
+                </ol>
+            </div>
+        </div>
+    </body>
+</html>
